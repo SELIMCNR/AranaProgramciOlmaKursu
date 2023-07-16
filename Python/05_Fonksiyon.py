ffa018b90f62f0ef997daebd81b79d3d52849b70
SELIMCNR <btk02672@gmail.com>
2023-07-17 00:06:18 +0300

    Fonksiyon yapısı

---
+++ Python/05_Fonksiyon.py
@@ -0,0 +1,7 @@
+def kredileriListele():
+    krediler = ["Hızlı kredi","Maaşını banktan alanlara özel","Mutlu kredi"]
+    for kredi in krediler:
+          print("<option>"+kredi+"<option>")
+
+kredileriListele()
+kredileriListele();
