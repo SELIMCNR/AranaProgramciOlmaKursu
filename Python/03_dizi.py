adf7e5b9a955bd2f1ebf1d0ceb12a6375c05b3aa
SELIMCNR <btk02672@gmail.com>
2023-07-16 23:58:32 +0300

    Python dizi yapısıı

---
+++ Python/03_dizi.py
@@ -0,0 +1,12 @@
+krediler = ["Hızlı Kredi","Maaşını Halkbanktan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+
+print(krediler)
+print(krediler[0])
+print(krediler[1])
+print(krediler[2])
+
+print(len(krediler))  # dizi uzunluğu
+
+krediler[0] = "Çabuk kredi"
+print(krediler)
+print(krediler[2])
