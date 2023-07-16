ed09498a807a344b32a476319a11fc2270ae7b2b
SELIMCNR <btk02672@gmail.com>
2023-07-16 23:47:41 +0300

    diğer kodlar

--- Python/01_Giris.py
+++ Python/01_Giris.py
@@ -1,7 +1,19 @@
-baslik = "haberiniz olsun"
-vade = 12 
-faizOranı =1.47
+baslik = "haberiniz olsun" #string
+vade = 12  # int
+faizOranı =1.47 #float
+faizOranı =1.44 #float
+
 print(baslik)
 print(type(baslik)) # type veri tipini söyler
 print(type(vade))
 print(type(faizOranı))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Engin"
+musteriSoyadi = "Demiroğ"
+sonucMesaj = mesaj + musteriAdi + musteriSoyadi
+print(sonucMesaj)
+
+sayi1 =10
+sayi2 = 20
+print(sayi1+sayi2)
