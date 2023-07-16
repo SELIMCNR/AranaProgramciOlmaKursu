2fbe99db991b364a727741b8d0776505905edc89
SELIMCNR <btk02672@gmail.com>
2023-07-17 00:14:41 +0300

    String Fonksiyonlar

---
+++ Python/Python-Dersleri-tam/02_String_Fonksiyonlar.py
@@ -0,0 +1,10 @@
+#first string
+firstString = "python is awesome!"
+
+#secon string
+secondString = "PyThOn Is AwEsOmE!"
+
+if(firstString.upper() == secondString.upper()):
+    print("The strings are same.")
+else:
+    print("The strings are not same.")    
