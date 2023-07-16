7a6147d073effda29d1de093cff3034450ab9f3b
SELIMCNR <btk02672@gmail.com>
2023-07-17 00:10:48 +0300

    Değişkenler

--- Python/Python-Dersleri-tam/01_Degiskenler.py
+++ Python/Python-Dersleri-tam/01_Degiskenler.py
@@ -1,4 +1,13 @@
 sayi = 10 # integer
 deger = "10" #string
-
+tc = "415616546616" #string
 print(10+sayi)
+isim = "selim"
+soyisim = "çınar"
+telefonNo = "46465465465"
+floatSayi = 10.0
+
+print(float(tc)+10)
+print(type(floatSayi))
+print(type(sayi))
+print(type(tc))
