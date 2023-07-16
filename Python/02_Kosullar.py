d36ac7ffafd73bcb47538b739c5a70ed52c97353
SELIMCNR <btk02672@gmail.com>
2023-07-16 23:52:54 +0300

    ilk kodlar koşul ifadeleri

---
+++ Python/02_Kosullar.py
@@ -0,0 +1,13 @@
+dolarDun = 7.85 
+dolarBugun = 7.75 
+
+if dolarDun > dolarBugun:
+    print("azalış oku")
+    print("bitti")
+
+if dolarDun< dolarBugun:
+    print("artış oku")
+
+
+if dolarDun == dolarBugun:
+    print("Eşittir oku")
